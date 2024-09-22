7a69cf53298cc9755f9f9abbf2c52717ae299a51
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-09-22 14:24:55 -0500

    Initial Set up

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Expense Tracker</h1>
+    <ExpenseForm @add-expense="addExpense" />
+    <ExpenseList :expenses="expenses" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import ExpenseForm from './components/ExpenseForm.vue';
+import ExpenseList from './components/ExpenseList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ExpenseForm,
+    ExpenseList,
+  },
+  setup() {
+    const expenses = ref([]);
+
+    const addExpense = (expense) => {
+      expenses.value.push(expense);
+    };
+
+    return {
+      expenses,
+      addExpense,
+    };
+  },
+};
+</script>
---
+++ src/components/ExpenseForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <form @submit.prevent="submitExpense">
+      <input v-model="title" placeholder="Expense Title" required />
+      <input v-model.number="amount" placeholder="Amount" required />
+      <button type="submit">Add Expense</button>
+    </form>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    name: 'ExpenseForm',
+    emits: ['add-expense'],
+    setup(props, { emit }) {
+      const title = ref('');
+      const amount = ref(0);
+  
+      const submitExpense = () => {
+        emit('add-expense', { title: title.value, amount: amount.value });
+        title.value = '';
+        amount.value = 0;
+      };
+  
+      return {
+        title,
+        amount,
+        submitExpense,
+      };
+    },
+  };
+  </script>
+  
